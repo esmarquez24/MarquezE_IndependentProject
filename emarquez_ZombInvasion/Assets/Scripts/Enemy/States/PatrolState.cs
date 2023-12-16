46d524894e32c7d416670a483bfcd41c51f71ce2
dollx <esmarquez24@gmail.com>
2023-12-16 08:45:35 -0800

    .

--- emarquez_ZombInvasion/Assets/Scripts/Enemy/States/PatrolState.cs
+++ emarquez_ZombInvasion/Assets/Scripts/Enemy/States/PatrolState.cs
@@ -6,6 +6,7 @@ public class PatrolState : BaseState
 {
     // track which waypoint we are currently targeting
     public int waypointIndex;
+    public float waitTimer;
     public override void Enter()
     {
     }
@@ -22,15 +23,20 @@ public class PatrolState : BaseState
         // implement patrol logic
         if (enemy.Agent.remainingDistance < 0.2f)
         {
-            if (waypointIndex < enemy.path.waypoints.Count -1)
+            waitTimer += Time.deltaTime;
+            if (waitTimer > 3)
             {
-                waypointIndex++;
+                if (waypointIndex < enemy.path.waypoints.Count - 1)
+                {
+                    waypointIndex++;
+                }
+                else
+                {
+                    waypointIndex = 0;
+                }
+                enemy.Agent.SetDestination(enemy.path.waypoints[waypointIndex].position);
+                waitTimer = 0;
             }
-            else
-            {
-                waypointIndex = 0;
-            }
-            enemy.Agent.SetDestination(enemy.path.waypoints[waypointIndex].position);
         }
     }
 }
